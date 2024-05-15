6688f06ffae5d98cfc44f008fa444ca8e164458e
JeanCarlos <jmatochepascual@gmail.com>
2024-05-14 19:51:31 -0500

    [FEARTURE] cargarRecetas

--- js/app.js
+++ js/app.js
@@ -23,7 +23,20 @@ const mostrarCategorias = (categorias) => {
   });
 };
 
+
+// Carga las recetas
+const cargarRecetas = () => {
+  const categoria = categoriasSelectElement.value;
+  const URL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
+
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.meals);
+    });
+};
 // Cargar Eventos
 document.addEventListener('DOMContentLoaded', () => {
   cargarCategorias();
+  categoriasSelectElement.addEventListener('change', cargarRecetas);
 });
